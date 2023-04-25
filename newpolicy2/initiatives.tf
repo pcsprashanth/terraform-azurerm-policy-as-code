302abde8140ddffbc32c72c3a306a8be5c9ad863
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-04-25 17:42:20 +0530

    define

--- newpolicy2/initiatives.tf
+++ newpolicy2/initiatives.tf
@@ -1,7 +1,7 @@
-module "Azure Security Benchmark" {
+module "Azure_Security_Benchmark" {
   source                  = "..//modules/initiative"
-  initiative_name         = "Azure Security Benchmark"
-  initiative_display_name = "[Security]: Azure Security BenchMark"
+  initiative_name         = "Azure_Security_Benchmark"
+  initiative_display_name = "[Security123]: Azure Security BenchMark"
   initiative_description  = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v3, see https://aka.ms/azsecbm. This also serves as the Microsoft Defender for Cloud default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Microsoft Defender for Cloud."
   initiative_category     = "Security Center"
 }
