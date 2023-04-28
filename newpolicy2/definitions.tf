39714cb09e7b7cb8b81e2ebee72d1d112732a54e
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-04-28 17:19:52 +0530

    merge

--- newpolicy2/definitions.tf
+++ newpolicy2/definitions.tf
@@ -17,3 +17,11 @@ module "whitelist_resources" {
   display_name        = "Allowed resources"
   policy_category     = "General"
 }
+module "deny_nat_rules_firewalls" {
+
+  source              = "..//modules/definition"
+  policy_name         = "deny_nat_rules_firewalls"
+  display_name        = "deny_nat_rules_firewalls"
+  policy_category     = "General"
+
+}
--- newpolicy2/initiatives.tf
+++ newpolicy2/initiatives.tf
@@ -1,6 +1,6 @@
 module "Azure_Security_Benchmark" {
   source                  = "..//modules/initiative"
-  initiative_name         = "Azure_Security_Benchmark"
+  initiative_name         = "Azure_Security1_Benchmark"
   initiative_display_name = "[Security123]: Azure Security BenchMark"
   initiative_description  = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v3, see https://aka.ms/azsecbm. This also serves as the Microsoft Defender for Cloud default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Microsoft Defender for Cloud."
   initiative_category     = "Security Center"
@@ -8,6 +8,7 @@ module "Azure_Security_Benchmark" {
  # Populate member_definitions with a for loop (explicit)
   member_definitions = [
     module.whitelist_regions.definition,
-    module.whitelist_resources.definition
+    module.whitelist_resources.definition,
+    module.deny_nat_rules_firewalls.definition
   ]
 }
