ea7c5c25fa7cac860454b439b30ffda85383272a
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-04-29 08:20:02 +0530

    new initiative

--- newpolicy2/definitions.tf
+++ newpolicy2/definitions.tf
@@ -4,3 +4,17 @@ module "deny_nic_public_ip" {
   display_name        = "Deny Azure Resource types"
   policy_category     = "General"
  }
+
+module "whitelist_regions" {
+  source              = "..//modules/definition"
+  policy_name         = "whitelist_regions"
+  display_name        = "Whitelist Azure Regions"
+  policy_category     = "General"
+}
+
+module "storage_min_tls" {
+  source              = "..//modules/definition"
+  policy_name         = "storage_min_tls"
+  display_name        = "Storage Accounts should have min tls"
+  policy_category     = "General"
+}
